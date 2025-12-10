1bd7e96e3e45abebe557f9280817e6706629035e
gurminder-cr <gurminder@coderroots.com>
2025-12-10 12:45:19 +0530

    population data

---
+++ to.py
@@ -0,0 +1,12 @@
+# import plyer
+
+# def send_notification(title, message):
+#     plyer.notification.notify(
+#         title=title,
+#         message=message,
+#         app_name="My Todo List",
+#         timeout=10  # Notification display duration in seconds
+#     )
+
+# # Example usage for a to-do item
+# send_notification("Task Reminder", "Don't forget to complete the report by 5 PM!")
